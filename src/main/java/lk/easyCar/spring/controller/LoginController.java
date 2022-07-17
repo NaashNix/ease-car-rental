d9df77c55dc8fa6ca9b5eb3f338311e5a18b5728
NaashNix <thilakshanaravindu@gmail.com>
2022-07-17 22:24:17 +0530

    Some Configurations Done

---
+++ src/main/java/lk/easyCar/spring/controller/LoginController.java
@@ -0,0 +1,14 @@
+package lk.easyCar.spring.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("app/login")
+@CrossOrigin
+public class LoginController {
+
+//    public void
+
+}
---
+++ src/main/java/lk/easyCar/spring/repo/LoginRepo.java
@@ -0,0 +1,6 @@
+package lk.easyCar.spring.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LoginRepo extends JpaRepository {
+}
