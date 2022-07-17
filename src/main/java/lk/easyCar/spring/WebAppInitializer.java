a23ab7a6d8c9def45c9669d09c50679b763d4c26
NaashNix <thilakshanaravindu@gmail.com>
2022-07-17 21:07:47 +0530

    files initialized

---
+++ src/main/java/lk/easyCar/spring/WebAppInitializer.java
@@ -0,0 +1,19 @@
+package lk.easyCar.spring;
+
+import lk.easyCar.spring.config.WebAppConfig;
+import lk.easyCar.spring.config.WebRootConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebRootConfig.class};
+    }
+
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebAppConfig.class};
+    }
+
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
---
+++ src/main/java/lk/easyCar/spring/config/WebRootConfig.java
@@ -0,0 +1,7 @@
+package lk.easyCar.spring.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebRootConfig {
+}
