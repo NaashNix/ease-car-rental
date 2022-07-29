d98b0f9568be6289d6303aad8db9cbb9cc2c9a40
NaashNix <thilakshanaravindu@gmail.com>
2022-07-29 17:24:40 +0530

    Registration page created

---
+++ Front_End/scripts/guest_page.js
@@ -0,0 +1,34 @@
+function loginButtonClicked() {
+    const username = $("#userNameField").val();
+    const password =  $("#passwordField").val();
+
+    if (username == '') {
+        $("#userNameField").css('border','1px solid red');
+    }else if (password == ''){
+        $("#userNameField").css('border', '');
+        $("#passwordField").css('border', '1px solid red');
+    }else{
+        $("#passwordField").css('border', '');
+    }
+
+    redirectToLoginProcess(username,password);
+
+}
+
+const redirectToLoginProcess = (username,password) => {
+    $.ajax({
+        type: "GET",
+        url: `http://localhost:8080/easy-car/app/login?username=${username}&password=${password}`,
+        data: "",
+        success: function (response) {
+            console.log(response);
+
+            // redirect to the place order form.
+        }
+    });
+}
+
+
+function loadCreateNewAccountPage() {
+    
+}
