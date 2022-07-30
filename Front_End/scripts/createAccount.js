a633d73f0462615c38ad1beb80bd4a2790d86478
NaashNix <thilakshanaravindu@gmail.com>
2022-07-30 23:49:51 +0530

    Create New Account UI done

--- Front_End/scripts/createAccount.js
+++ Front_End/scripts/createAccount.js
@@ -1,13 +1,32 @@
 const basicInfoForm = $("#create-account-form-parent");
 const imageUploadForm = $("#id-upload-form-parent");
+const credentialSettingForm = $("#credential-setting-form");
+
+let x = 0;
+if (x == 0) {
+    imageUploadForm.css('display', 'none');
+    credentialSettingForm.css('display', 'none');
+    credentialSettingForm.css('display', 'none');
+    x = 1;
+}
 
 
-imageUploadForm.css('display', 'none');
 
- 
 
-function loadIdImageUploadPage () {
+
+function loadIdImageUploadPage() {
     basicInfoForm.addClass('horizTranslate');
-    setTimeout(() => {basicInfoForm.css ('display','none')}, 700);
+    setTimeout(() => { basicInfoForm.css('display', 'none'); }, 700);
     imageUploadForm.addClass('verticalIntro');
+}
+
+function loadCredentialSettingPage() {
+    imageUploadForm.addClass('horizTranslate');
+    setTimeout(() => {
+        imageUploadForm.css('display', 'none');
+        imageUploadForm.removeClass('verticalIntro'); 
+        credentialSettingForm.addClass('verticalIntro');
+    }, 700);
+    
+
 }
